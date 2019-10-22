57699b94319d59952615c371d19986bb6838798f
Adilet Momunaliev and Trevor Mackin <trevor@>
2019-10-22 14:48:55 -0700

    It will begin the Flash Card Project. It will open new image and css directory. It will download Bootstrap and Jquery. It will add flashcard content to index.html. It will link all files to html.

---
+++ js/scripts.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+
+  $("#javascript").click(function(){
+      $(".javascript-p").toggle();
+  });
+
+  $("#operators").click(function(){
+      $(".operators-p").toggle();
+  });
+
+  $("#variables").click(function(){
+      $(".variables-p").toggle();
+  });
+
+  $("#variable-name").click(function(){
+      $(".variable-name-p").toggle();
+  });
+
+  $("#functions").click(function(){
+      $(".functions-p").toggle();
+  });
+
+  $("#methods").click(function(){
+      $(".methods-p").toggle();
+  });
+
+  $("#arguments").click(function(){
+      $(".arguments-p").toggle();
+  });
+
+  $("#parameters").click(function(){
+    $(".parameters-p").toggle();
+  });
+
+  $("#return").click(function(){
+    $(".return-p").toggle();
+  });
+
+  $("#chaining-methods").click(function(){
+    $(".chaining-methods-p").toggle();
+  });
+
+  $("#strings").click(function(){
+    $(".strings-p").toggle();
+  });
+
+  $("#booleans").click(function(){
+    $(".booleans-p").toggle();
+  });
+
+  $("#undefined").click(function(){
+    $(".undefined-p").toggle();
+  });
+
+  $("#nan").click(function(){
+    $(".nan-p").toggle();
+  });
+
+  $("#escape").click(function(){
+    $(".escape-p").toggle();
+  });
+
+  $("#alert").click(function(){
+    $(".alert-p").toggle();
+  });
+
+  $("#comments").click(function(){
+    $(".comments-p").toggle();
+  });
+
+  $("#jquery").click(function(){
+    $(".jquery-p").toggle();
+  });
+
+  $("#attributes").click(function(){
+    $(".attributes-p").toggle();
+  });
+
+
+});
